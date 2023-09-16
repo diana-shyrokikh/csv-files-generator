fa241160c4a381a44affa25af29df8fc001a5d7d
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-16 17:58:07 +0300

    :hammer: Added  DataSchemaListView

--- csv_generator/views.py
+++ csv_generator/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from csv_generator.models import DataSchema
+
+
+class DataSchemaListView(ListView):
+    model = DataSchema
+    template_name = "data_schema_list.html"
+    context_object_name = "data_schemas"
+
+    def get_queryset(self):
+        return DataSchema.objects.filter(user=self.request.user)
