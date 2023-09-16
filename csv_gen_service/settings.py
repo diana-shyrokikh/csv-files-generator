bff0f73c8d06d603fc0a3832b778ca3ed64ff984
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-16 13:51:09 +0300

    :wrench: Added app and auth user model in settings.py

--- csv_gen_service/settings.py
+++ csv_gen_service/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "csv_generator",
 ]
 
 MIDDLEWARE = [
@@ -102,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "csv_generator.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
