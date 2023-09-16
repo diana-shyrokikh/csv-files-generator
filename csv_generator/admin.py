0af033486b0ff8be5678698eb64d550e43388de0
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-16 13:51:44 +0300

    :hammer: Added models in admin.py

--- csv_generator/admin.py
+++ csv_generator/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, SchemaColumn, DataSchema
+
+
+class SchemaColumnInLine(admin.TabularInline):
+    model = SchemaColumn
+    extra = 1
+
+
+@admin.register(DataSchema)
+class DataSchemaAdmin(admin.ModelAdmin):
+    inlines = [SchemaColumnInLine]
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+admin.site.register(SchemaColumn)
