e6e62ca46bcd8973072a89fb0693730635d02c57
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-17 18:00:50 +0300

    :hammer: Added path to DataSchemaCreate Page

--- csv_generator/urls.py
+++ csv_generator/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from csv_generator.views import DataSchemaListView
+from csv_generator.views import DataSchemaListView, DataSchemaCreateView
 
 app_name = "csv_generator"
 
 urlpatterns = [
-    path("schemas/", DataSchemaListView.as_view(), name="data_schemas"),
+    path("schemas/", DataSchemaListView.as_view(), name="schema-list"),
+    path("schemas/create/", DataSchemaCreateView.as_view(), name="schema-create"),
 ]
