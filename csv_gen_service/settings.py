8951cb10409b7ef579da20cb233c693fb398bc40
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-21 14:55:08 +0300

    :wrench: Added celery settings in settings.py

--- csv_gen_service/settings.py
+++ csv_gen_service/settings.py
@@ -134,3 +134,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+
