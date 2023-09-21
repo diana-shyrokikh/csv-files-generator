f487ad6a5b99b6caa59e28c1b0d3a23a6cb4f37d
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-21 20:39:31 +0300

    :hammer: Added success_url to DataSchemaCreateView in views.py

--- csv_generator/views.py
+++ csv_generator/views.py
@@ -1,8 +1,17 @@
-from django.urls import reverse_lazy
+import os
+
+from django.conf import settings
+from django.http import FileResponse
+from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views import View
 from django.views.generic import ListView, CreateView
 
-from csv_generator.forms import DataSchemaForm, SchemaColumnFormSet
-from csv_generator.models import DataSchema
+from csv_generator.forms import (
+    DataSchemaForm,
+    SchemaColumnFormSet,
+)
+from csv_generator.models import DataSchema, GeneratedCSV
 
 
 class DataSchemaListView(ListView):
@@ -11,7 +20,9 @@ class DataSchemaListView(ListView):
     context_object_name = "data_schemas"
 
     def get_queryset(self):
-        return DataSchema.objects.filter(user=self.request.user)
+        return DataSchema.objects.filter(
+            user=self.request.user
+        )
 
 
 class DataSchemaCreateView(CreateView):
@@ -23,17 +34,64 @@ class DataSchemaCreateView(CreateView):
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
+
         if self.request.POST:
-            data["children"] = SchemaColumnFormSet(self.request.POST, prefix="schema_column")
+            data["children"] = SchemaColumnFormSet(
+                self.request.POST, prefix="schema_column"
+            )
         else:
-            data["children"] = SchemaColumnFormSet(prefix="schema_column")
+            data["children"] = SchemaColumnFormSet(
+                prefix="schema_column"
+            )
         return data
 
     def form_valid(self, form):
         context = self.get_context_data()
         children = context["children"]
         self.object = form.save()
+
         if children.is_valid():
             children.instance = self.object
             children.save()
+
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse(
+            "csv_generator:csv-generate",
+            kwargs={'pk': self.object.pk}
+        )
+
+
+class CSVGenerateView(View):
+    @staticmethod
+    def get(request, pk):
+        data_schema = DataSchema.objects.get(id=pk)
+        generated_csv_files = GeneratedCSV.objects.filter(
+            data_schema_id=pk
+        )
+        context = {
+            "data_schema": data_schema,
+            "generated_csv_files": generated_csv_files
+        }
+
+        return render(request, "generator_csv.html", context)
+
+
+class CSVDownloadView(View):
+    @staticmethod
+    def get(request, pk):
+        csv_file = GeneratedCSV.objects.get(pk=pk)
+        file_path = os.path.join(
+            settings.MEDIA_ROOT, csv_file.file.name
+        )
+        filename = f"{csv_file.data_schema.title}.csv"
+        response = FileResponse(
+            open(file_path, "rb"),
+            as_attachment=True,
+        )
+        response[
+            "Content-Disposition"
+        ] = f"attachment; filename={filename}"
+
+        return response
