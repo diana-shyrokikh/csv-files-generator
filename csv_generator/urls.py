85635249b92494a7ea2b318417564ade43757220
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-21 20:34:47 +0300

    :hammer: Added urls for generate and download csv file views in urls.py

--- csv_generator/urls.py
+++ csv_generator/urls.py
@@ -1,10 +1,33 @@
 from django.urls import path
 
-from csv_generator.views import DataSchemaListView, DataSchemaCreateView
+from csv_generator.views import (
+    DataSchemaListView,
+    DataSchemaCreateView,
+    CSVGenerateView,
+    CSVDownloadView,
+)
 
 app_name = "csv_generator"
 
 urlpatterns = [
-    path("schemas/", DataSchemaListView.as_view(), name="schema-list"),
-    path("schemas/create/", DataSchemaCreateView.as_view(), name="schema-create"),
+    path(
+        "",
+        DataSchemaListView.as_view(),
+        name="schema-list"
+    ),
+    path(
+        "create/",
+        DataSchemaCreateView.as_view(),
+        name="schema-create"
+    ),
+    path(
+        "generate/<int:pk>/",
+        CSVGenerateView.as_view(),
+        name="csv-generate"
+    ),
+    path(
+        "generate/<int:pk>/download/",
+        CSVDownloadView.as_view(),
+        name="csv-download"
+    ),
 ]
