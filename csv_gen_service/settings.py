87cbd1ba2101fb486d5ae37e2d8e3a1b20c4ee14
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-21 15:18:41 +0300

    :wrench: Added DB postreSQL settings in settings.py

--- csv_gen_service/settings.py
+++ csv_gen_service/settings.py
@@ -86,9 +86,13 @@ WSGI_APPLICATION = 'csv_gen_service.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": 'postgres',
+        "PORT": '5432',
     }
 }
 
