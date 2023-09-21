43ad4d8e5f6e70cca18add82003d4b9b538f7b83
Diana Shyrokikh <diashiroks@gmail.com>
2023-09-21 15:04:34 +0300

    :sparkles: Added websocket in asgi.py

--- csv_gen_service/asgi.py
+++ csv_gen_service/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csv_gen_service.settings')
+import csv_generator.routing
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csv_gen_service.settings")
+
+application = ProtocolTypeRouter({
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+                URLRouter(
+                        csv_generator.routing.websocket_urlpatterns
+                )
+        ),
+})
